6ec7229e97cf5964584d074cfd5e63a936961884
unknown <viktoriia.savchuk@infis.cz>
2025-03-19 13:08:15 +0100

    huhuhu

--- zadani1.py
+++ zadani1.py
@@ -9,5 +9,16 @@ print(f"Ahoj, jmenuji se {jmeno} a je mi {vek} let.")
 #ukol2
 prvniCislo = float(input("Zadejte prvni cislo: "))
 druheCislo = float(input("Zadejte druhe cislo: "))
-print(f"{(prvniCislo*druheCislo)}:)
-      
+
+soucet = prvniCislo + druheCislo
+rozdil = prvniCislo - druheCislo
+soucin = prvniCislo * druheCislo
+podil = prvniCislo/druheCislo
+
+print(f"Soucet: {soucet:.2f}")
+print(f"Rozdil: {rozdil:.2f}")
+print(f"Soucin: {soucin:.2f}")
+print(f"Podil: {podil:.2f}")
+
+
+
