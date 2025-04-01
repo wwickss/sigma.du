53c2727da9eafad19ae6e2cc207a7b98912028d9
unknown <viktoriia.savchuk@infis.cz>
2025-04-01 09:21:03 +0200

    testik fruits

---
+++ samostatnaPrace.py
@@ -0,0 +1,14 @@
+fruits = ["Banana","Apple","Orange","Pear","Kiwi","Peach"]
+print(len(fruits))
+for i in range(len(fruits)):
+    print(fruits[i])
+fruits.append("Lemon")
+print(fruits)
+fruits.pop(1)
+print(fruits)
+fruits.sort()
+print(fruits)
+fruits.reverse()
+print(fruits)
+
+
